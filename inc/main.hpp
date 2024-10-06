6b1531f9cdacddc4c4434f86052c4f2e33bf74ad
Daniel-Pena-96 <118245331+Daniel-Pena-96@users.noreply.github.com>
2024-10-07 00:00:41 +0200

    classes experiments

--- inc/main.hpp
+++ inc/main.hpp
@@ -1,1 +1,32 @@
-/*This is just a test*/
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <string>
+
+class Person {
+private:
+
+    int weight;
+
+public:
+
+    std::string name;
+    int age;
+
+    Person();
+    Person(const std::string& name, int age, int weight)
+    {
+        this->name = name;
+        this->age = age;
+        this->weight = weight;
+    }
+
+    int getWeight() const{
+        return weight;
+    }
+    void setAge(int newAge){
+        this->age = newAge;
+    }
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "main.hpp"
+
+Person bro("Cancho", 27, 70);
+Person colleague("Viktor", 30, 80);
 
 int main(){
-
-    std::cout << "Hello world" << std::endl;
-    std::cout << "It worked!" << std::endl;
+    std::cout << "age is a public   member / colleague age: " << colleague.age << std::endl;
+    colleague.setAge(31);
+    std::cout << "name is a public member " << bro.name << std::endl;
+    std::cout << "weight is private member " << bro.getWeight() << std::endl;
+    std::cout << "age is a public   member / new colleague age: " << colleague.age << std::endl;
+    
     return 0;
 }
